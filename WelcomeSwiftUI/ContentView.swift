da4c77f88d832d3c4bdd8a5f1831c356df4c5f9b
Muthuraj Muthulingam - Vendor <Muthuraj.Muthulingam@walmart.com>
2019-07-11 13:58:53 +0530

    Add Swift UI

--- WelcomeSwiftUI/ContentView.swift
+++ WelcomeSwiftUI/ContentView.swift
@@ -9,9 +9,35 @@
 import SwiftUI
 
 struct ContentView : View {
-    var body: some View {
-        Text("Hello World")
+    
+    var persons: [Person] {
+        get {
+            return [.init(id: 1, name: "Muthu", age: "28", gender: .male)]
+        }
     }
+    
+    var body: some View {
+        NavigationView {
+            List(persons.identified(by: \.id)) { person in
+                Text("Welcome \(person.name) to SwiftUI List ")
+            }
+            .padding(EdgeInsets(top: 10, leading: 10, bottom: 10, trailing: 10))
+        }
+        .navigationBarTitle(Text("Muthu"), displayMode: .inline)
+    }
+}
+
+enum Gender {
+    case male
+    case female
+    case transgender
+}
+
+struct Person: Identifiable {
+    var id: Int
+    let name: String
+    let age: String
+    let gender: Gender
 }
 
 #if DEBUG
