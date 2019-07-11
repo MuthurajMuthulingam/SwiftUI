a6809d7094a4efdfe1ef8375fedbbd507cb45307
Muthuraj Muthulingam - Vendor <Muthuraj.Muthulingam@walmart.com>
2019-07-11 10:55:12 +0530

    Initial Commit

---
+++ WelcomeSwiftUI/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  WelcomeSwiftUI
+//
+//  Created by Muthuraj Muthulingam - Vendor on 11/07/19.
+//  Copyright © 2019 Muthuraj Muthulingam - Vendor. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
